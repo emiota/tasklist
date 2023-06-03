68ee6cdba05b2b705341fbc0e326d2250934d3f7
User <User@DESKTOP-P97G1T5>
2023-06-04 03:18:24 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -46,6 +46,11 @@ public class EditServlet extends HttpServlet {
         // メッセージIDをセッションスコープに登録
         request.getSession().setAttribute("task_id", m.getId());
 
+     // メッセージデータが存在しているときのみ
+        // メッセージIDをセッションスコープに登録
+        if(m != null) {
+            request.getSession().setAttribute("task_id", m.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/task/edit.jsp");
         rd.forward(request, response);
